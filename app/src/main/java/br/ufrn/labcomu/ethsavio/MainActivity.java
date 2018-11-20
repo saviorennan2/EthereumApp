ab11f6eefb610b614d5cb32348723307e9510eea
pdnog <saviorennan@gmail.com>
2018-11-20 12:20:26 -0300

    criando e lendo credentials

--- app/src/main/java/br/ufrn/labcomu/ethsavio/MainActivity.java
+++ app/src/main/java/br/ufrn/labcomu/ethsavio/MainActivity.java
@@ -1,7 +1,6 @@
 package br.ufrn.labcomu.ethsavio;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -15,8 +14,6 @@ import android.widget.Toast;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.datatypes.Function;
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.RawTransaction;
@@ -32,25 +29,18 @@ import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
 import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.Transfer;
-import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
 
 import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import rx.Subscription;
 
 import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
@@ -69,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
     private BigInteger nonce;
     private PersonalUnlockAccount personalUnlockAccount;
     String walletPath;
+    String fileName;
+    File path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,12 +71,34 @@ public class MainActivity extends AppCompatActivity {
             checkPermissions();
         }
 
+
+        web3 = Web3jFactory.build(new HttpService("http://192.168.1.17:8545"));
+        web3j = AdminFactory.build(new HttpService("http://192.168.1.17:8545"));
+
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions, 101);
+            }
+        }
+    }
+
+    public void createWallet(View view) {
+
         synchronized (this) {
             // Create new wallet
 
-                String path = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS).getPath();
+            path = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS);
+            if (!path.exists()) {
+                path.mkdir();
+            }
             try {
-                String fileName = WalletUtils.generateLightNewWalletFile("password", new File(path));
+                fileName = WalletUtils.generateLightNewWalletFile("senha", new File(path.getPath()));
+                Log.d("FILE", fileName);
             } catch (NoSuchAlgorithmException e) {
                 e.printStackTrace();
             } catch (NoSuchProviderException e) {
@@ -100,39 +114,10 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        web3 = Web3jFactory.build(new HttpService("http://192.168.1.17:8545"));
-        web3j = AdminFactory.build(new HttpService("http://192.168.1.17:8545"));
-
-        //CREATE WALLET
-        try {
-            walletPath = WalletUtils.generateFullNewWalletFile("senha", walletPathFile);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        } catch (CipherException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
-    private void checkPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                String[] permissions = new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                requestPermissions(permissions, 101);
-            }
-        }
-    }
+    public void loadCredentials(View view) throws IOException {
 
-    public void createWallet(View view) throws IOException, ExecutionException, InterruptedException {
-//        String walletPath = WalletUtils.generateFullNewWalletFile("yourownpassword", walletPathFile);
-//        Toast.makeText(this, "Wallet created successfully.", Toast.LENGTH_SHORT).show();
-        // using a raw transaction
         BigInteger nonce = BigInteger.valueOf(25);
         BigInteger gasprice = BigInteger.valueOf(0);
         BigInteger gaslimit = BigInteger.valueOf(1048575);
@@ -147,36 +132,16 @@ public class MainActivity extends AppCompatActivity {
         Credentials credentials = null;
 
         try {
-            credentials = WalletUtils.loadCredentials("senha", "/sdcard/EthWallet/");
+            credentials = WalletUtils.loadCredentials("senha", path.getPath() + "/" + fileName);
+            Log.d("CREDENTIALS", credentials.getAddress());
         } catch (CipherException e) {
             e.printStackTrace();
         }
-
-        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
-        String hexValue = Numeric.toHexString(signedMessage);
-
-        EthSendTransaction ethSendTransaction = web3.ethSendRawTransaction(hexValue).sendAsync().get();
-        String transactionHash = ethSendTransaction.getTransactionHash();
-
-        EthGetTransactionReceipt transactionReceipt =
-                web3.ethGetTransactionReceipt(transactionHash).send();
-
-
-        String contractAddress = transactionReceipt.getTransactionReceipt().getContractAddress();
-        Log.d("Contract", contractAddress);
 
     }
 
     public void connectEth(View view) throws ExecutionException, InterruptedException {
-        try {
-            Credentials credentials = WalletUtils.loadCredentials(
-                    "password",
-                    "/storage/emulated/0/Download/UTC--2018-03-16T19-05-15.125--833e56c5df2a654372a252658006af4d3158e9f3.json");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CipherException e) {
-            e.printStackTrace();
-        }
+
         new WebClientTask().execute("");
 
     }
@@ -272,16 +237,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void loadCredentials(View view) throws IOException, CipherException {
-        new WebClientTask().execute("");
 
-//        credentials =
-//                WalletUtils.loadCredentials(
-//                        "Your wallet password",
-//                        "Your wallet stored path");
-//        log.info("Credentials loaded Address is::: " + credentials.getAddress());
-
-    }
 
     public void transferFunds(View view) {
         new TransactionTask().execute("");
